29572dc7879354a757ab105f70ad68439bfdd7ef
MiguelRendon <rendonmg@gmail.com>
2022-08-27 13:03:11 -0500

    tweets_post_and_show

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def sign_up(user: UserRegister = Body(...)):
         results.append(user_dict)
 
         f.seek(0)
-        f.write(json.dumps(results))
+        f.write(json.dumps(results))        
         return user
 
 @app.post(
@@ -65,7 +65,7 @@ def login():
     pass
 
 
-@app.post(
+@app.get(
     path="/users",
     response_model=List[User],
     status_code=status.HTTP_200_OK,
@@ -80,7 +80,7 @@ def show_all_users():
         - 
     
     Returns a JSON list with all the users in the app, with the following structure:
-    
+
         - user_id: UUID
         - email: EmailStr
         - first_name: str
@@ -136,13 +136,29 @@ def home():
 
 @app.get(
     path="/tweets",
-    response_model=Tweet,
+    response_model=List[Tweet],
     status_code=status.HTTP_200_OK,
     tags=["Tweets"],
     summary="Lists tweets"
 )
 def show_tweets():
-    pass
+    """
+    This path operation shows all tweets in the app
+
+    Parameters:
+        - 
+    
+    Returns a JSON list with all the tweets in the app, with the following structure:
+
+        - tweet_id: UUID 
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.load(f)
+        return results
 
 @app.post(
     path="/tweets",
@@ -151,8 +167,40 @@ def show_tweets():
     tags=["Tweets"],
     summary="Posts a new tweet"
 )
-def post_tweet():
-    pass
+def post_tweet(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+
+    This path operation posts a new tweet in the app
+    
+    Parameters:
+
+        - Request body parameter
+            - tweet: Tweet
+    
+    Returns a JSON with basic tweet information:
+    
+        - tweet_id: UUID 
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.load(f)
+                
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])        
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        results.append(tweet_dict)
+
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 
 @app.get(
