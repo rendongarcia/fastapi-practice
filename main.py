3e25832197dec46feac7ef7c319d3f431f8a80da
MiguelRendon <rendonmg@gmail.com>
2022-08-27 10:24:49 -0500

    models_user

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
+# Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+from pydantic import validator
+
 # FastAPI
 from fastapi import FastAPI, status
+from fastapi import Body
 
 app = FastAPI()
+
+
+# Models
+class UserBase(BaseModel):    
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+    @validator("birth_date")  # Aqui está la magia
+    def is_over_eighteen(cls, v):
+        todays_date = date.today()
+        delta = todays_date - v
+
+        if delta.days/365 <= 18:
+            raise ValueError("Must be over 18!")
+        else:
+            return v
+
+
+class Tweet(BaseModel):
+    pass
+
 
 @app.get(
     path="/",
@@ -9,3 +63,4 @@ app = FastAPI()
     tags=["Home"])
 def home():
     return {"Twitter API": "Running!"}
+
